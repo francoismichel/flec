ee8d4d7fcb20e227247a2d13d71f6df56394fa04
François Michel <francois.michel@uclouvain.be>
2019-09-11 00:16:54 +0200

    small update and populate code.plugin of simple_fec

--- plugins/simple_fec/protoops/post_write_src_fpi_frame.c
+++ plugins/simple_fec/protoops/post_write_src_fpi_frame.c
@@ -6,7 +6,7 @@
 
 // we here assume a single-path context
 
-protoop_arg_t pre_write_fpi_frame(picoquic_cnx_t *cnx) {
+protoop_arg_t post_write_src_fpi_frame(picoquic_cnx_t *cnx) {
     plugin_state_t *state = get_plugin_state(cnx);
     if (!state)
         return PICOQUIC_ERROR_MEMORY;
