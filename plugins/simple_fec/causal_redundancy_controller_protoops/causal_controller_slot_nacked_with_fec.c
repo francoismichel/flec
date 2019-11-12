d34074fadb8be95c31980fc781c8dd811cf8352f
François Michel <francois.michel@uclouvain.be>
2019-11-12 15:59:05 +0100

    FEC: use a separate slock (n)acked event when we use the FEC plugin that sends forward coded packets

---
+++ plugins/simple_fec/causal_redundancy_controller_protoops/causal_controller_slot_nacked_with_fec.c
@@ -0,0 +1,14 @@
+
+#include <picoquic.h>
+#include <getset.h>
+#include "../fec.h"
+#include "../window_framework/types.h"
+#include "causal_redundancy_controller_new.h"
+#include "../window_framework/framework_sender.h"
+
+protoop_arg_t causal_slot_nacked(picoquic_cnx_t *cnx) {
+    window_redundancy_controller_t controller = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 0);
+    uint64_t slot = (source_symbol_id_t) get_cnx(cnx, AK_CNX_INPUT, 1);
+    slot_nacked(cnx, controller, slot);
+    return 0;
+}
