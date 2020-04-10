d462d7f2b286368d73bb34d6dcf9859e1aa2b6bd
François Michel <francois.michel@uclouvain.be>
2020-04-10 13:38:04 +0200

    ensure to not reserve FEC ID frame when connection flow-control blocked

--- plugins/simple_fec/protoops/has_protected_data_to_send.c
+++ plugins/simple_fec/protoops/has_protected_data_to_send.c
@@ -1,5 +1,6 @@
 #include <picoquic.h>
 #include <getset.h>
+#include <picoquic_internal.h>
 #include "../../helpers.h"
 #include "../fec_constants.h"
 #include "../fec.h"
@@ -10,5 +11,15 @@
  */
 protoop_arg_t has_protected_data_to_send(picoquic_cnx_t *cnx)
 {
-    return helper_find_ready_stream(cnx) != NULL;
+    picoquic_stream_head *stream = helper_find_ready_stream(cnx);
+    if (stream == NULL) {
+        return false;
+    }
+    picoquic_stream_data *data = (picoquic_stream_data *) get_stream_head(stream, AK_STREAMHEAD_SEND_QUEUE);
+    bool cnx_fc_blocked = get_cnx(cnx, AK_CNX_MAXDATA_REMOTE, 0) <= get_cnx(cnx, AK_CNX_DATA_SENT, 0);
+    return data != NULL && ((!cnx_fc_blocked && get_stream_data(data, AK_STREAMDATA_LENGTH) >= get_stream_data(data, AK_STREAMDATA_OFFSET))
+                         || (get_stream_data(data, AK_STREAMDATA_LENGTH) == get_stream_data(data, AK_STREAMDATA_OFFSET)
+                                            && (PSTREAM_FIN_NOTIFIED(stream) && !PSTREAM_FIN_SENT(stream)) &&
+                                               (PSTREAM_RESET_REQUESTED(stream) && !PSTREAM_RESET_SENT(stream)) &&
+                                               (PSTREAM_STOP_SENDING_REQUESTED(stream) && !PSTREAM_STOP_SENDING_SENT(stream))) );
 }
