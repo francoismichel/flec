0648d0307cf68be6f096c183affa359b0e77c7be
François Michel <francois.michel@uclouvain.be>
2020-08-10 14:04:51 +0200

    rewrite process_ack_range for perfs

--- plugins/simple_fec/protoops/process_ack_range.c
+++ plugins/simple_fec/protoops/process_ack_range.c
@@ -30,16 +30,17 @@ protoop_arg_t process_ack_range(picoquic_cnx_t *cnx)
             } else {
                 if (sequence_number == highest) {
                     /* TODO: RTT Estimate */
+                    uint32_t length = (uint32_t) get_pkt(p, AK_PKT_LENGTH);
                     picoquic_packet_t* next = (picoquic_packet_t*) get_pkt(p, AK_PKT_NEXT_PACKET);
                     picoquic_path_t * old_path = (picoquic_path_t *) get_pkt(p, AK_PKT_SEND_PATH);
-                    set_path(old_path, AK_PATH_DELIVERED, 0, get_path(old_path, AK_PATH_DELIVERED, 0) + get_pkt(p, AK_PKT_LENGTH));
+                    set_path(old_path, AK_PATH_DELIVERED, 0, get_path(old_path, AK_PATH_DELIVERED, 0) + length);
 
-                    uint32_t length = (uint32_t) get_pkt(p, AK_PKT_LENGTH);
                     if ((picoquic_congestion_algorithm_t *) get_cnx(cnx, AK_CNX_CONGESTION_CONTROL_ALGORITHM, 0) != NULL) {
                         helper_congestion_algorithm_notify(cnx, old_path,
                                                            picoquic_congestion_notification_acknowledgement, 0, length, 0, current_time);
                     }
-                    packet_has_been_acknowledged(cnx, old_path, sequence_number, get_pkt(p, AK_PKT_SEND_TIME), current_time);
+                    uint64_t send_time = get_pkt(p, AK_PKT_SEND_TIME);
+                    packet_has_been_acknowledged(cnx, old_path, sequence_number, send_time, current_time);
                     bool fec_protected = FEC_PKT_IS_FEC_PROTECTED(get_pkt_metadata(cnx, p, FEC_PKT_METADATA_FLAGS));
                     bool contains_repair_frame = FEC_PKT_CONTAINS_REPAIR_FRAME(get_pkt_metadata(cnx, p, FEC_PKT_METADATA_FLAGS));
                     if (fec_protected || contains_repair_frame) {
@@ -47,8 +48,8 @@ protoop_arg_t process_ack_range(picoquic_cnx_t *cnx)
                         source_symbol_id_t id = (source_symbol_id_t) get_pkt_metadata(cnx, p, FEC_PKT_METADATA_FIRST_SOURCE_SYMBOL_ID);
                         uint16_t n_source_symbols = (uint16_t) get_pkt_metadata(cnx, p, FEC_PKT_METADATA_NUMBER_OF_SOURCE_SYMBOLS);
                         fec_packet_symbols_have_been_received(cnx, sequence_number, slot, id, n_source_symbols,
-                                                              fec_protected, contains_repair_frame, get_pkt(p, AK_PKT_SEND_TIME), current_time);
-                        PROTOOP_PRINTF(cnx, "[[PACKET RECEIVED]] %lu,%lu\n", sequence_number, current_time - get_pkt(p, AK_PKT_SEND_TIME));
+                                                              fec_protected, contains_repair_frame, send_time, current_time, &state->pid_received_packet);
+                        PROTOOP_PRINTF(cnx, "[[PACKET RECEIVED]] %lu,%lu\n", sequence_number, current_time - send_time);
                     }
 
                     /* If the packet contained an ACK frame, perform the ACK of ACK pruning logic */
@@ -95,7 +96,7 @@ protoop_arg_t process_ack_range(picoquic_cnx_t *cnx)
         } else if (closest_lost->pn <= highest) { // closest_lost is always not null as p == NULL
             if (closest_lost->pn == highest) {
                 PROTOOP_PRINTF(cnx, "ALREADY LOST SYMBOL RECEIVED\n");
-                fec_packet_symbols_have_been_received(cnx, closest_lost->pn, closest_lost->slot, closest_lost->id, closest_lost->n_source_symbols, true, false, closest_lost->send_time, current_time);
+                fec_packet_symbols_have_been_received(cnx, closest_lost->pn, closest_lost->slot, closest_lost->id, closest_lost->n_source_symbols, true, false, closest_lost->send_time, current_time, &state->pid_received_packet);
                 dequeue_lost_packet(cnx, &state->lost_packets, closest_lost->pn, NULL, NULL, NULL, NULL);
             }
             range--;
