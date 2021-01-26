0085daada4a5a455ff1e7fb5c0d44d771c9fbd8e
François Michel <francois.michel@uclouvain.be>
2021-01-26 19:04:31 +0100

    FEC: fixes and optimization for window_reserve_repair_frames

--- plugins/simple_fec/window_framework/protoops/window_reserve_repair_frames.c
+++ plugins/simple_fec/window_framework/protoops/window_reserve_repair_frames.c
@@ -20,7 +20,7 @@ protoop_arg_t reserve_repair_frames_protoop(picoquic_cnx_t *cnx) {
     window_source_symbol_id_t first_id_to_protect = (window_source_symbol_id_t) get_cnx(cnx, AK_CNX_INPUT, 5);
     uint16_t n_symbols_to_protect = (uint16_t) get_cnx(cnx, AK_CNX_INPUT, 6);
     int max_repair_frames_threshold = 50;
-    int client_max_repair_frames_threshold = 2;
+    int client_max_repair_frames_threshold = -1;
 //    int max_repair_frames_threshold = 10;
 //    if (!is_lost_packet_queue_empty(cnx, &state->lost_packets))
 //        max_repair_frames_threshold = wff->window_length*2;
@@ -42,8 +42,8 @@ protoop_arg_t reserve_repair_frames_protoop(picoquic_cnx_t *cnx) {
 
         PROTOOP_PRINTF(cnx, "NOTHING TO RESERVE, GENERATE !\n");
         if (!is_fec_window_empty(wff)) {
-            PROTOOP_PRINTF(cnx, "GENERATE %d REPAIR SYMBOLS, FEEDBACK-IMPLIED = %d, PROTECT SUBSET = %d, FIRST = %u, n_symbols = %u\n", PICOQUIC_MAX_PACKET_SIZE/state->symbol_size/* + 1*/, feedback_implied, protect_subset, first_id_to_protect, n_symbols_to_protect);
-            err = generate_and_queue_repair_symbols(cnx, wff, true, PICOQUIC_MAX_PACKET_SIZE/state->symbol_size /*+ 1*/, state->symbol_size, protect_subset, first_id_to_protect, n_symbols_to_protect);
+            PROTOOP_PRINTF(cnx, "GENERATE %d REPAIR SYMBOLS, FEEDBACK-IMPLIED = %d, PROTECT SUBSET = %d, FIRST = %u, n_symbols = %u\n", MAX(1, PICOQUIC_MAX_PACKET_SIZE/state->symbol_size)/* + 1*/, feedback_implied, protect_subset, first_id_to_protect, n_symbols_to_protect);
+            err = generate_and_queue_repair_symbols(cnx, wff, true, MAX(1, PICOQUIC_MAX_PACKET_SIZE/state->symbol_size) /*+ 1*/, state->symbol_size, protect_subset, first_id_to_protect, n_symbols_to_protect);
             if (!err)
                 err = window_reserve_repair_frames(cnx, wff, max_size, symbol_size, feedback_implied);
             if (!err) {
