5ad3ce31a5bcb630de30a6b3ab437a221a063438
François Michel <francois.michel@uclouvain.be>
2021-01-26 18:48:08 +0100

    FEC: simple variable renaming

--- plugins/simple_fec/window_framework/protoops/window_parse_recovered_frame.c
+++ plugins/simple_fec/window_framework/protoops/window_parse_recovered_frame.c
@@ -22,12 +22,12 @@ protoop_arg_t parse_frame(picoquic_cnx_t *cnx) {
     bytes_protected++;
 
     size_t consumed = 0;
-    uint8_t *size_and_packets = parse_window_recovered_frame(cnx, bytes_protected, bytes_max, &consumed);
+    uint8_t *size_and_ranges = parse_window_recovered_frame(cnx, bytes_protected, bytes_max, &consumed);
 
-    if (!size_and_packets)  // error
+    if (!size_and_ranges)  // error
         return (protoop_arg_t) NULL;
 
-    set_cnx(cnx, AK_CNX_OUTPUT, 0, (protoop_arg_t) size_and_packets);
+    set_cnx(cnx, AK_CNX_OUTPUT, 0, (protoop_arg_t) size_and_ranges);
     set_cnx(cnx, AK_CNX_OUTPUT, 1, true);
     set_cnx(cnx, AK_CNX_OUTPUT, 2, true);
     return (protoop_arg_t) bytes_protected + consumed;
