855c2518efdd6e48820251ed0297c36d81b87bc5
François Michel <francois.michel@uclouvain.be>
2021-01-26 17:20:27 +0100

    FEC: avoid sending stream frames when repair frames are waiting due to ACKs

--- plugins/simple_fec/protoops/stream_bytes_max_no_stream_frame_with_repair_frame.c
+++ plugins/simple_fec/protoops/stream_bytes_max_no_stream_frame_with_repair_frame.c
@@ -12,7 +12,7 @@ protoop_arg_t stream_bytes_max(picoquic_cnx_t* cnx) {
     plugin_state_t *state = get_plugin_state(cnx);
     if (!state)
         return -1;
-    if (state->has_written_repair_frame || state->has_written_fb_fec_repair_frame)
+    if (state->has_written_repair_frame || state->has_written_fb_fec_repair_frame || state->retried_repair_for_ack)
         // avoid having a stream frame with a repair frame
         bytes_max = 0;
     else {
@@ -21,7 +21,7 @@ protoop_arg_t stream_bytes_max(picoquic_cnx_t* cnx) {
 //        size_t overhead = 1 + (1 + sizeof(uint64_t)) + sizeof(fec_frame_header_t);
 //        bytes_max = (bytes_max && bytes_max > overhead) ? bytes_max-overhead : bytes_max;
         size_t overhead = 25;
-        uint64_t mtu = get_path((picoquic_path_t *) get_cnx(cnx, AK_CNX_PATH, 0), AK_PATH_SEND_MTU, 0) - 30; // max repair frame size
+        uint64_t mtu = get_path((picoquic_path_t *) get_cnx(cnx, AK_CNX_PATH, 0), AK_PATH_SEND_MTU, 0) - 5; // max repair frame size
         uint64_t max_pkt_size = MIN(CHUNK_SIZE, mtu);   // 30 is an ok value for header + checksum overheads
         bytes_max = (bytes_max && bytes_max > max_pkt_size - overhead) ? (max_pkt_size-overhead) : (bytes_max-overhead);
     }
