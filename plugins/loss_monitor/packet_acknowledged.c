57c8735c0ce7db936148e8384b6f5817d285a27b
François Michel <francois.michel@uclouvain.be>
2021-01-26 14:42:56 +0100

    LOSS MONITOR: anchor on packed_acknowledged instead of packet_acked

--- plugins/loss_monitor/packet_acknowledged.c
+++ plugins/loss_monitor/packet_acknowledged.c
@@ -4,10 +4,12 @@
 protoop_arg_t monitor_packet_acknowledged(picoquic_cnx_t *cnx) {
 
 
-    picoquic_path_t *path = (picoquic_path_t *) get_cnx(cnx, AK_CNX_INPUT, 0);
-    int64_t packet_number = (int64_t) get_cnx(cnx, AK_CNX_INPUT, 1);
-    uint64_t send_time = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 2);
-    uint64_t current_time = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 3);
+    picoquic_packet_t *p = (picoquic_packet_t *) get_cnx(cnx, AK_CNX_INPUT, 0);
+    uint64_t current_time = (uint64_t) get_cnx(cnx, AK_CNX_INPUT, 1);
+
+    picoquic_path_t *path = (picoquic_path_t *) get_pkt(p, AK_PKT_SEND_PATH);
+    uint64_t send_time = (uint64_t) get_pkt(p, AK_PKT_SEND_TIME);
+    int64_t packet_number = (int64_t) get_pkt(p, AK_PKT_SEQUENCE_NUMBER);
 
     loss_monitor_t *monitor = (loss_monitor_t *) get_path_metadata(cnx, path, PATH_METADATA_LOSS_MONITOR_IDX);
     PROTOOP_PRINTF(cnx, "ACK GOT MONITOR %p\n", (protoop_arg_t) monitor);
