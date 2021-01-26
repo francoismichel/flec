7dce7a532ddeae61bf9c33b85cf49d6122fe83f4
François Michel <francois.michel@uclouvain.be>
2021-01-26 16:58:54 +0100

    FEC: optimize pre_process_ack_range and call sub protoop to handle received symbols signal

--- plugins/simple_fec/protoops/pre_process_ack_range.c
+++ plugins/simple_fec/protoops/pre_process_ack_range.c
@@ -22,24 +22,24 @@ protoop_arg_t process_ack_range(picoquic_cnx_t *cnx)
     picoquic_packet_t* p = ppacket;
     int ret = 0;
     lost_packet_t *closest_lost = NULL;
+    closest_lost = get_smallest_lost_packet_equal_or_bigger(cnx, &state->lost_packets, highest - range + 1);
+    int64_t sequence_number = -1L;
+    if (p != NULL) {
+        sequence_number = get_pkt(p, AK_PKT_SEQUENCE_NUMBER);
+    }
     /* Compare the range to the retransmit queue */
-    while (((closest_lost = get_lost_packet_equal_or_smaller(cnx, &state->lost_packets, highest)) && closest_lost->pn >= 0) && range > 0) {
-        // if p is not nul, p is not in the lost packets and vice-versa
-        if (p != NULL) {
-            uint64_t sequence_number = get_pkt(p, AK_PKT_SEQUENCE_NUMBER);
-            if (sequence_number <= highest) {
-                range--;
-                highest--;
-            }
-            p = (picoquic_packet_t*) get_pkt(p, AK_PKT_NEXT_PACKET);
-        } else if (closest_lost->pn <= highest) { // closest_lost is always not null as p == NULL
-            if (closest_lost->pn == highest) {
-                fec_packet_symbols_have_been_received(cnx, closest_lost->pn, closest_lost->slot, closest_lost->id, closest_lost->n_source_symbols, true, false, closest_lost->send_time, current_time, &state->pid_received_packet);
-                dequeue_lost_packet(cnx, &state->lost_packets, closest_lost->pn, NULL, NULL, NULL, NULL);
-            }
-            range--;
-            highest--;
+    while ((closest_lost && closest_lost->pn >= 0) && range > 0) {
+        // if p is not null, p is not in the lost packets and vice-versa
+
+        if (highest - range < closest_lost->pn && closest_lost->pn <= highest) {
+            // received
+            fec_packet_symbols_have_been_received(cnx, closest_lost->pn, closest_lost->slot, closest_lost->id, closest_lost->n_source_symbols, true, false, closest_lost->send_time, current_time, &state->pid_received_packet);
+            dequeue_lost_packet(cnx, &state->lost_packets, closest_lost->pn, NULL, NULL, NULL, NULL);
+            closest_lost = get_smallest_lost_packet_equal_or_bigger(cnx, &state->lost_packets, highest - range + 1);
+        } else {
+            break;
         }
     }
+    PROTOOP_PRINTF(cnx, "PRE PROCESS ACK RANGE ELAPSED %luµs\n", picoquic_current_time() - now);
     return (protoop_arg_t) ret;
 }
