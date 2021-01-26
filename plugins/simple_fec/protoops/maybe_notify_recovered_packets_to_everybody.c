37fb1223ef30f9d7cb27eb167fcfd85455cb95e1
François Michel <francois.michel@uclouvain.be>
2021-01-26 16:46:09 +0100

    FEC: update maybe_notify_recovered_packets_to_everybody protoop

--- plugins/simple_fec/protoops/maybe_notify_recovered_packets_to_everybody.c
+++ plugins/simple_fec/protoops/maybe_notify_recovered_packets_to_everybody.c
@@ -7,9 +7,10 @@ protoop_arg_t prepare_packet_ready(picoquic_cnx_t *cnx)
     plugin_state_t *state = get_plugin_state(cnx);
     if (!state) return PICOQUIC_ERROR_MEMORY;
 
-    PROTOOP_PRINTF(cnx, "MAYBE NOTIFY PACKETS, SIZE = %lu\n", state->recovered_packets.size);
-
-    if (state->recovered_packets.size)
-        maybe_notify_recovered_packets_to_everybody(cnx, &state->recovered_packets, current_time);
+//    PROTOOP_PRINTF(cnx, "MAYBE NOTIFY PACKETS, SIZE = %lu\n", rbt_size(cnx, &state->recovered_packets_ranges));
+    uint64_t now = picoquic_current_time();
+    if (!rbt_is_empty(cnx, &state->recovered_packets_ranges))
+        maybe_notify_recovered_packets_to_everybody(cnx, &state->recovered_packets_ranges, current_time);
+//    PROTOOP_PRINTF(cnx, "NOTIFICATION ELAPSED = %luµs\n", picoquic_current_time() - now);
     return 0;
 }
