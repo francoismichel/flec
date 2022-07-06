e3c050f58095eb783854426abbc8e0fcaee83e2b
François Michel <francois.michel@uclouvain.be>
2022-07-06 17:15:06 +0200

    set the fixed CC cwin to 10000000

--- plugins/disable_congestion_control/congestion_notify.c
+++ plugins/disable_congestion_control/congestion_notify.c
@@ -6,7 +6,7 @@
 
 
 
-#define CWIN 300000
+#define CWIN 10000000
 
 /**
  * See PROTOOP_NOPARAM_CONGESTION_ALGORITHM_NOTIFY
@@ -14,9 +14,6 @@
 protoop_arg_t congestion_algorithm_notify(picoquic_cnx_t *cnx)
 {
     picoquic_path_t* path_x = (picoquic_path_t *) get_cnx(cnx, AK_CNX_INPUT, 0);
-    uint64_t configured_cwin = get_cnx_metadata(cnx, 0);
-    uint64_t cwin = (configured_cwin != 0) ? configured_cwin : CWIN;
-    set_path(path_x, AK_PATH_CWIN, 0, cwin);
-    PROTOOP_PRINTF(cnx, "EVENT::{\"time\": %ld, \"type\": \"cwin\", \"cwin\": %lu}\n", picoquic_current_time(), cwin);
+    set_path(path_x, AK_PATH_CWIN, 0, CWIN);
     return 0;
 }
