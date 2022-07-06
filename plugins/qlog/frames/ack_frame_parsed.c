e5c4393e0afb90dd2f39362a68cb5e0bbb4b4405
François Michel <francois.michel@uclouvain.be>
2022-07-06 17:09:43 +0200

    handle longer ack blocks in qlog

--- plugins/qlog/frames/ack_frame_parsed.c
+++ plugins/qlog/frames/ack_frame_parsed.c
@@ -2,10 +2,15 @@
 
 #define BLOCK_STR_LEN 1200
 
+#ifndef MAX
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
 protoop_arg_t protoop_log(picoquic_cnx_t *cnx) {
     TMP_FRAME_BEGIN_MALLOC(cnx, parsed_frame, frame, ack_frame_t)
         {
-            char *block_str = my_malloc(cnx, 1200);
+            size_t block_str_len = BLOCK_STR_LEN;
+            char *block_str = my_malloc(cnx, block_str_len);
             if (!block_str)
                 return 0;
             size_t ack_ofs = 0;
@@ -19,13 +24,43 @@ protoop_arg_t protoop_log(picoquic_cnx_t *cnx) {
                 } else {
                     range = frame->ack_blocks[num_block].additional_ack_block + 1;
                 }
+                int written;
+                ssize_t size = block_str_len - ack_ofs;
+                if (range <= 1) {
+                    written = snprintf(block_str + ack_ofs, size, "[\"%" PRIu64 "\"]", largest);
+                    if (written >= size) {  // block_str was too small: resize it
+                        ssize_t block_str_new_len = MAX(block_str_len*2, block_str_len + written);
+                        char *block_str_new = my_malloc(cnx, block_str_new_len);
+                        if (!block_str_new)
+                            return 0;
+                        my_memcpy(block_str_new, block_str, block_str_len);
+                        my_free(cnx, block_str);
+                        block_str = block_str_new;
+                        block_str_len = block_str_new_len;
+                        size = block_str_len - ack_ofs;
+                        // we redo the writing, with the correct buffer size now
+                        written = snprintf(block_str + ack_ofs, size, "[\"%" PRIu64 "\"]", largest);
+                    }
+                }
+                else {
+                    written = snprintf(block_str + ack_ofs, size, "[\"%" PRIu64 "\", \"%" PRIu64 "\"]", largest - range + 1, largest);
+                    if (written >= size) {  // block_str was too small: resize it
+                        ssize_t block_str_new_len = MAX(block_str_len*2, block_str_len + written);
+                        char *block_str_new = my_malloc(cnx, block_str_new_len);
+                        if (!block_str_new)
+                            return 0;
+                        my_memcpy(block_str_new, block_str, block_str_len);
+                        my_free(cnx, block_str);
+                        block_str = block_str_new;
+                        block_str_len = block_str_new_len;
+                        size = block_str_len - ack_ofs;
+                        // we redo the writing, with the correct buffer size now
+                        written = snprintf(block_str + ack_ofs, size, "[\"%" PRIu64 "\", \"%" PRIu64 "\"]", largest - range + 1, largest);
+                    }
+                }
+                ack_ofs += written;
 
-                if (range <= 1)
-                    ack_ofs += snprintf(block_str + ack_ofs, BLOCK_STR_LEN - ack_ofs, "[\"%" PRIu64 "\"]", largest);
-                else
-                    ack_ofs += snprintf(block_str + ack_ofs, BLOCK_STR_LEN - ack_ofs, "[\"%" PRIu64 "\", \"%" PRIu64 "\"]", largest - range + 1, largest);
-
-                ack_ofs += snprintf(block_str + ack_ofs, BLOCK_STR_LEN - ack_ofs, num_block == ack_block_count - 1 ? "" : ", ");
+                ack_ofs += snprintf(block_str + ack_ofs, block_str_len - ack_ofs, num_block == ack_block_count - 1 ? "" : ", ");
 
                 if (num_block == ack_block_count - 1)
                     break;
@@ -36,10 +71,10 @@ protoop_arg_t protoop_log(picoquic_cnx_t *cnx) {
                 largest -= block_to_block;
             }
             block_str[ack_ofs] = 0;
-            char *ack_str = my_malloc(cnx, BLOCK_STR_LEN + 200);
+            char *ack_str = my_malloc(cnx, block_str_len + 200);
             if (!ack_str)
                 return 0;
-            PROTOOP_SNPRINTF(cnx, ack_str, BLOCK_STR_LEN + 200, "{\"frame_type\": \"ack\", \"ack_delay\": \"%" PRIu64 "\", \"acked_ranges\": [%s]}", frame->ack_delay, (protoop_arg_t) block_str);
+            PROTOOP_SNPRINTF(cnx, ack_str, block_str_len + 200, "{\"frame_type\": \"ack\", \"ack_delay\": \"%" PRIu64 "\", \"acked_ranges\": [%s]}", frame->ack_delay, (protoop_arg_t) block_str);
             helper_log_frame(cnx, ack_str);
             my_free(cnx, block_str);
             my_free(cnx, ack_str);
